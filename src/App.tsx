c02dcaea88664b9fe190f6c0a7fb048fde4f6b8c
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-02-15 23:16:02 +0700

    add azaret font

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className='bg-[#1A1A2E] w-full min-h-screen'>
         <nav className='w-full'>
-          <h1 className='text-white'>HanR</h1>
+          <h1 className='text-white font-azaret text-2xl'>Hello World</h1>
         </nav>
       </div>
     </>
