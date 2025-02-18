c0b7760c563fc8556b4d3319f53d5b630c7ae8f1
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-02-18 19:42:54 +0700

    wip: not found pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
   },
+  { path: "*", element: <NotFound /> },
 ]);
 
 const App: React.FC = () => {
