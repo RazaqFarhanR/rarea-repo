187ace729b54aedd5ecdec0cee73ffb55885cf8a
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-02-19 23:38:12 +0700

    wip: journey section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -103,14 +103,14 @@ function App() {
 
       <div className="px-8 lg:px-52 my-12">
         <h1 className="text-2xl lg:text-4xl font-azaret font-semibold text-white">The Journey so Far</h1>
-        <div className="grid gap-4 h-max">
+        <div className="grid gap-4 relative after:rounded-xl after:-ml-[6px] after:w-4 after:absolute after:bg-[#16213E] after:h-full after:top-0 after:left-1/2">
           {(() => {
             let rows = [];
-            for (let i = 0; i < 6; i++) {
+            for (let i = 0; i < 4; i++) {
               if (i%2!=0) {
                 rows.push(
                   <div className="z-10 grid grid-cols-9 gap-2">
-                    <div className="col-start-5 bg-green-200">
+                    <div className="col-start-5">
       
                     </div>
                     <div className="col-span-4 col-end-10 bg-red-200 h-32 rounded-lg">
@@ -124,7 +124,7 @@ function App() {
                     <div className="col-span-4 bg-red-200 h-32 rounded-lg">
 
                     </div>
-                    <div className="bg-green-200">
+                    <div className="">
 
                     </div>
                   </div>
