f36736becdca32e3f3408dae68d9e75dadd6eeac
RazaqFarhanR <aljawadfarhan@gmail.com>
2025-02-20 00:28:27 +0700

    fix: margin main section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div className='bg-[#1A1A2E] w-full min-h-screen h-auto'>
-      <header className='w-full flex justify-between items-center px-4 py-2 lg:px-16 lg:py-5'>
+      <header className='w-full flex justify-between items-center m-auto max-w-[172ch] px-8 py-2 lg:py-5'>
         <h1 className='text-white font-azaret text-3xl lg:text-5xl'>HanR</h1>
         <nav className="flex space-x-4">
           <Link to="/" className="text-white font-azaret text-lg lg:text-2xl list-none">Home</Link>
@@ -14,125 +14,125 @@ function App() {
         </nav>
       </header>
 
-      <div className="px-8 grid grid-cols-1 md:px-12 lg:px-52 md:grid-cols-8 mt-2">
-        <div className="col-span-5 py-12 lg:py-20">
-          <div className="bg-[#16213E] p-4 lg:p-8 rounded-xl border-4 border-white">
-            <h1 className="text-2xl lg:text-5xl font-azaret font-semibold text-white">Hello World</h1>
-            <p className="text-xl lg:text-4xl font-azaret font-semibold text-white">I'm <span className="text-[#E94560]">Razaq Farhan</span></p>
-            <div className="h-14 my-2 text-ellipsis overflow-hidden">
-              <p className="text-white text-xs lg:text-lg font-azaret leading-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div className="flex mt-4 justify-between">
-              <button className="bg-[#E94560] text-white py-1 px-3 text-sm rounded-md lg:text-base lg:py-2 lg:px-12 lg:rounded-xl font-azaret border-2 border-transparent hover:bg-transparent hover:border-2 hover:!border-[#E94560] hover:text-[#E94560] active:scale-95 transition-all">about me</button>
-            </div>
-          </div>
-        </div>
-        <div className="hidden md:flex md:items-center md:col-span-3 md:ml-8">
-          <img className="scale-x-[-1] w-full" src={room} alt="about-gif" />
-        </div>
-      </div>
-
-      <div className="px-8 lg:px-52 lg:my-12">
-        <h1 className="text-2xl lg:text-4xl font-azaret font-semibold text-white">Languages & Tools</h1>
-        <div className="bg-[#16213E] w-full flex justify-center flex-wrap p-2 gap-3 border-4 rounded-xl mt-2 lg:!px-0 lg:gap-[6px]">
-          <Icon name="windows" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="ubuntu" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="nginx" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="git" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="vscode" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="arduino" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="js" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="ts" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="golang" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="nodejs" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="express" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="bun" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="react" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="next" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="tailwind" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="mysql" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="mariadb" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="postgresql" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-          <Icon name="figma" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
-        </div>
-      </div>
-
-      <div className="px-8 lg:px-52 my-12">
-        <h1 className="text-2xl lg:text-4xl font-azaret font-semibold text-white">Featured Projects</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 mt-4">
-          <div className="p-3 w-[100%] bg-[#16213E] rounded-xl">
-            <img className="w-full" src={Project} alt="project-img"/>
-            <div className="">
-              <h1 className="text-2xl text-white font-azaret font-bold mt-2">Project 1</h1>
-              <div className="mt-4 h-16 text-ellipsis overflow-hidden">
-                <p className="text-white text-sm font-azaret leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div className="m-auto max-w-[140ch] px-8">
+        <div className="grid grid-cols-1 md:grid-cols-8 mt-2">
+          <div className="col-span-5 py-12 lg:py-20">
+            <div className="bg-[#16213E] p-4 lg:p-8 rounded-xl border-4 border-white">
+              <h1 className="text-2xl lg:text-5xl font-azaret font-semibold text-white">Hello World</h1>
+              <p className="text-xl lg:text-4xl font-azaret font-semibold text-white">I'm <span className="text-[#E94560]">Razaq Farhan</span></p>
+              <div className="h-14 my-2 text-ellipsis overflow-hidden">
+                <p className="text-white text-xs lg:text-lg font-azaret leading-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
-              <div className="mt-4 grid grid-cols">
-                <p className="bg-[#E94560] py-1 px-6 w-fit rounded-full text-[#1A1A2E] font-bold text-base">React</p>
+              <div className="flex mt-4 justify-between">
+                <button className="bg-[#E94560] text-white py-1 px-3 text-sm rounded-md lg:text-base lg:py-2 lg:px-12 lg:rounded-xl font-azaret border-2 border-transparent hover:bg-transparent hover:border-2 hover:!border-[#E94560] hover:text-[#E94560] active:scale-95 transition-all">about me</button>
               </div>
             </div>
           </div>
-          
-          <div className="p-3 w-[100%] bg-[#16213E] rounded-xl">
-            <img className="w-full" src={Project} alt="project-img"/>
-            <div className="">
-              <h1 className="text-2xl text-white font-azaret font-bold mt-2">Project 1</h1>
-              <div className="mt-4 h-16 text-ellipsis overflow-hidden">
-                <p className="text-white text-sm font-azaret leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              </div>
-              <div className="mt-4 grid grid-cols">
-                <p className="bg-[#E94560] py-1 px-6 w-fit rounded-full text-[#1A1A2E] font-bold text-base">React</p>
+          <div className="hidden md:flex md:items-center md:col-span-3 md:ml-8">
+            <img className="scale-x-[-1] w-full" src={room} alt="about-gif" />
+          </div>
+        </div>
+
+        <div className="lg:my-12">
+          <h1 className="text-2xl lg:text-4xl font-azaret font-semibold text-white">Languages & Tools</h1>
+          <div className="bg-[#16213E] w-full flex justify-center flex-wrap p-2 gap-3 border-4 rounded-xl mt-2 lg:!px-0 lg:gap-[6px]">
+            <Icon name="windows" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="ubuntu" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="nginx" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="git" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="vscode" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="arduino" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="js" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="ts" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="golang" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="nodejs" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="express" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="bun" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="react" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="next" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="tailwind" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="mysql" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="mariadb" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="postgresql" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+            <Icon name="figma" className="w-8 h-8 lg:w-[52px] lg:h-[52px]"/>
+          </div>
+        </div>
+
+        <div className="my-12">
+          <h1 className="text-2xl lg:text-4xl font-azaret font-semibold text-white">Featured Projects</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 mt-4">
+            <div className="p-3 w-[100%] bg-[#16213E] rounded-xl">
+              <img className="w-full" src={Project} alt="project-img"/>
+              <div className="">
+                <h1 className="text-2xl text-white font-azaret font-bold mt-2">Project 1</h1>
+                <div className="mt-4 h-16 text-ellipsis overflow-hidden">
+                  <p className="text-white text-sm font-azaret leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div className="mt-4 grid grid-cols">
+                  <p className="bg-[#E94560] py-1 px-6 w-fit rounded-full text-[#1A1A2E] font-bold text-base">React</p>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div className="p-3 w-[100%] bg-[#16213E] rounded-xl">
-            <img className="w-full" src={Project} alt="project-img"/>
-            <div className="">
-              <h1 className="text-2xl text-white font-azaret font-bold mt-2">Project 1</h1>
-              <div className="mt-4 h-16 text-ellipsis overflow-hidden">
-                <p className="text-white text-sm font-azaret leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            
+            <div className="p-3 w-[100%] bg-[#16213E] rounded-xl">
+              <img className="w-full" src={Project} alt="project-img"/>
+              <div className="">
+                <h1 className="text-2xl text-white font-azaret font-bold mt-2">Project 1</h1>
+                <div className="mt-4 h-16 text-ellipsis overflow-hidden">
+                  <p className="text-white text-sm font-azaret leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div className="mt-4 grid grid-cols">
+                  <p className="bg-[#E94560] py-1 px-6 w-fit rounded-full text-[#1A1A2E] font-bold text-base">React</p>
+                </div>
               </div>
-              <div className="mt-4 grid grid-cols">
-                <p className="bg-[#E94560] py-1 px-6 w-fit rounded-full text-[#1A1A2E] font-bold text-base">React</p>
+            </div>
+
+            <div className="p-3 w-[100%] bg-[#16213E] rounded-xl">
+              <img className="w-full" src={Project} alt="project-img"/>
+              <div className="">
+                <h1 className="text-2xl text-white font-azaret font-bold mt-2">Project 1</h1>
+                <div className="mt-4 h-16 text-ellipsis overflow-hidden">
+                  <p className="text-white text-sm font-azaret leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div className="mt-4 grid grid-cols">
+                  <p className="bg-[#E94560] py-1 px-6 w-fit rounded-full text-[#1A1A2E] font-bold text-base">React</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="px-8 lg:px-52 my-12">
-        <h1 className="text-2xl lg:text-4xl font-azaret font-semibold text-white">The Journey so Far</h1>
-        <div className="grid gap-4 relative after:rounded-xl after:-ml-[6px] after:w-4 after:absolute after:bg-[#16213E] after:h-full after:top-0 after:left-1/2">
-          {(() => {
-            let rows = [];
-            for (let i = 0; i < 4; i++) {
-              if (i%2!=0) {
-                rows.push(
-                  <div className="z-10 grid grid-cols-9 gap-2">
-                    <div className="col-start-5">
-      
-                    </div>
-                    <div className="col-span-4 col-end-10 bg-red-200 h-32 rounded-lg">
-      
-                    </div>
-                  </div>
-                );
-              } else {
-                rows.push(
-                  <div className="z-10 grid grid-cols-9 gap-2">
-                    <div className="col-span-4 bg-red-200 h-32 rounded-lg">
+        <div className="my-12">
+          <h1 className="text-2xl lg:text-4xl font-azaret font-semibold text-white mt-4">The Journey so Far</h1>
+          <div className="grid gap-4 relative after:rounded-xl after:-ml-[6px] after:w-4 after:absolute after:bg-[#16213E] after:h-full after:top-0 after:left-1/2">
+            {(() => {
+              let rows = [];
+              for (let i = 0; i < 4; i++) {
+                if (i%2!=0) {
+                  rows.push(
+                    <div className="z-10 relative flex justify-end">
+                      <div className="w-[56%] h-32 flex justify-end">
+                        <div className="bg-red-100 w-[78%] h-full rounded-xl">
 
+                        </div>
+                      </div>
                     </div>
-                    <div className="">
+                  );
+                } else {
+                  rows.push(
+                    <div className="z-10 relative flex justify-start">
+                      <div className="w-[56%] h-32 flex justify-start">
+                        <div className="bg-red-100 w-[78%] h-full rounded-xl">
 
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                )
+                  )
+                }
               }
-            }
-            return rows;
-          })()}
+              return rows;
+            })()}
+          </div>
         </div>
       </div>
 
